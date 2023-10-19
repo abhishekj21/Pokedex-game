f00092be686f27df5570121060c886477b234f0b
unknown <singhabhishekj21@gmail.com>
2023-10-19 17:18:46 +0100

    installed all dependencies

---
+++ src/components/Pokedex/Pokedex.jsx
@@ -0,0 +1,11 @@
+import Search from "../Search/Search";
+function Pokedex() {
+  return (
+    <>
+      Pokedex
+      <Search />
+    </>
+  );
+}
+
+export default Pokedex;
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,9 @@
+function Search() {
+  return (
+    <>
+      <input type="text" placeholder="pokemon name...." />
+    </>
+  );
+}
+
+export default Search;
